b10140abb2a265a93bd00eeab26a2996e54f1bce
jameswhatcott <james@whatcott.com>
2024-07-12 20:58:11 -0500

    added code to make the svg file appear

--- index.js
+++ index.js
@@ -6,14 +6,7 @@ const questions = [
     "Text Color",
     "Shape",
     "Shape's Color"
-     ];
-
-
-
-
-
-
-
+];
 
 function init() {
   
@@ -23,41 +16,65 @@ function init() {
       type: "input",
       message: `${questions[0]}`,
       name: "text",
+      validate: function (input) {
+        if (input.length > 3) {
+          return "Text must be 3 characters or less.";
+        }
+        return true;
+      }
     },
     {
         type: "input",
         message: `${questions[1]}`,
-        name: "text color"
+        name: "textColor"
     },
     {
         type: "list",
         message: `${questions[2]}`,
-        name: "shape"
+        name: "shape",
+        choices: ["Circle", "Square", "Triangle"]
     },
     {
         type: "input",
         message: `${questions[3]}`,
-        name: "shape color"
+        name: "shapeColor"
     }
     
   ])
   .then((response) => {
     console.log(response);
-    fs.writeFile('logo.svg', generateREADME(response), (err) =>
-    err ? console.error(err) : console.log('Success!')
+    fs.writeFile('logo.svg', generateLogo(response), (err) =>
+    err ? console.error(err) : console.log('Generated logo.svg')
     
   )});
 
 }
 
-
-
-
-
+function generateLogo(response) {
+  const { text, textColor, shape, shapeColor } = response;
+  
+  let shapeElement;
+  
+  switch (shape) {
+    case 'Circle':
+      shapeElement = `<circle cx="150" cy="100" r="80" fill="${shapeColor}" />`;
+      break;
+    case 'Square':
+      shapeElement = `<rect x="70" y="20" width="160" height="160" fill="${shapeColor}" />`;
+      break;
+    case 'Triangle':
+      shapeElement = `<polygon points="150,20 280,180 20,180" fill="${shapeColor}" />`;
+      break;
+    default:
+      shapeElement = '';
+  }
+  
+  return `
+<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+  ${shapeElement}
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+</svg>
+  `;
+}
 
 init();
-
-
-
-
-  
